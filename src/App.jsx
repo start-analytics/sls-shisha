8928cbd3d72086d1700ed5c8ee575f52cad92d59
reidizhupa <reidizhupa56@gmail.com>
2025-01-08 23:58:40 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,60 @@ import drinkImg from "./assets/img/bg43.jpg";
 import snackImg from "./assets/img/snacks.jpg";
 import systemImg from "./assets/img/img1.png";
 
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./components/Homepage";
+
+const router = createBrowserRouter([
+	{
+		path: "/sls-shisha",
+		element: <Homepage />,
+	},
+	{
+		path: "/sls-shisha/flavors",
+		element: (
+			<>
+				<Header page="flavors" />
+				<Hero img={shishaImg} height="60vh" />
+				<RecommendedMixes />
+				<Menu />
+				<Footer />
+			</>
+		),
+	},
+	{
+		path: "/sls-shisha/drinks",
+		element: (
+			<>
+				<Header page="drinks" />
+				<Hero img={drinkImg} height="60vh" />
+				<Drinks />
+				<Footer />
+			</>
+		),
+	},
+	{
+		path: "/sls-shisha/snacks",
+		element: (
+			<>
+				<Header page="snacks" />
+				<Hero img={snackImg} height="60vh" />
+				<Snacks />
+				<Footer />
+			</>
+		),
+	},
+	{
+		path: "/sls-shisha/system",
+		element: (
+			<>
+				<Header page="system" />
+				<Hero img={systemImg} height="60vh" />
+				<System />
+				<Footer />
+			</>
+		),
+	},
+]);
 
 function App() {
 	const [isMobileNavActive, setIsMobileNavActive] = useState(false);
@@ -36,65 +88,7 @@ function App() {
 			cleanupScrollspy();
 		};
 	}, []);
-	return (
-		<HashRouter>
-			<Routes>
-				<Route
-					path="/sls-shisha"
-					element={
-						<>
-							<Homepage />
-						</>
-					}
-				/>
-				<Route
-					path="/sls-shisha/flavors"
-					element={
-						<>
-							<Header page="flavors" />
-							<Hero img={shishaImg} height="60vh" />
-							<RecommendedMixes />
-							<Menu />
-							<Footer />
-						</>
-					}
-				/>
-				<Route
-					path="/sls-shisha/drinks"
-					element={
-						<>
-							<Header page="drinks" />
-							<Hero img={drinkImg} height="60vh" />
-							<Drinks />
-							<Footer />
-						</>
-					}
-				/>
-				<Route
-					path="/sls-shisha/snacks"
-					element={
-						<>
-							<Header page="snacks" />
-							<Hero img={snackImg} height="60vh" />
-							<Snacks />
-							<Footer />
-						</>
-					}
-				/>
-				<Route
-					path="/sls-shisha/system"
-					element={
-						<>
-							<Header page="system" />
-							<Hero img={systemImg} height="60vh" />
-							<System />
-							<Footer />
-						</>
-					}
-				/>
-			</Routes>
-		</HashRouter>
-	);
-}
 
+	return <RouterProvider router={router} />;
+}
 export default App;
